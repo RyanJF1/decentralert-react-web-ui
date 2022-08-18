296826a2579e9016bf9d44eaf44f45f6a327d056
Ryan Fulton <ryan.j.fulton@gmail.com>
2022-08-17 21:49:29 -0400

    package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import './i18n';
 import { BrowserRouter, Switch, Route,Redirect } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
