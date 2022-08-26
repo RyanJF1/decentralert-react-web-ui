b93d87178c201d6d522fb099accc6bd3b1d42e65
Ryan Fulton <ryan.j.fulton@gmail.com>
2022-08-26 16:59:13 -0400

    Added create account

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,11 +1,27 @@
 import React, {Fragment, useEffect, useState} from 'react';
 import Breadcrumb from '../common/breadcrumb';
 import axios from "axios";
-import {Button, Card, CardBody, CardImg, CardSubtitle, CardText, Input} from "reactstrap";
+import {
+    Button,
+    Card,
+    CardBody,
+    CardImg,
+    CardSubtitle,
+    CardText,
+    Input,
+    Modal,
+    ModalBody, ModalFooter,
+    ModalHeader
+} from "reactstrap";
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [modal, setModal] = useState(false);
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+
+    const toggle = () => setModal(!modal);
 
     const handleUsernameChange = event => {
         setUsername(event.target.value);
@@ -37,6 +53,32 @@ const Login = () => {
         localStorage.setItem("x-user-token", null);
         console.log(localStorage.getItem("x-user-token"));
     }
+    const handleNameChange = event => {
+        setName(event.target.value);
+    };
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value);
+    };
+
+    const addUser = (name, email, password) => {
+        axios.post(process.env.REACT_APP_API_URL + "/user", {
+
+            email: email,
+            name: name,
+            password: password
+        },{
+            headers: {
+                'X-API-KEY': process.env.REACT_APP_API_KEY
+            }})
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
         return(
             <div className="container">
                 <div className="form-box">
@@ -66,11 +108,45 @@ const Login = () => {
                                 />
                             </div>
                             <button type="button" className="btn btn-secondary btn-block" onClick={() => login(username, password)}>LOGIN</button>
-                            <button type="button" className="btn btn-secondary btn-block" onClick={() => logout()}>LOGOUT</button>
+                            <button type="button" className="btn btn-danger btn-block" onClick={() => logout()}>LOGOUT</button>
+                            <Button color="primary" onClick={toggle}>
+                                Create account
+                            </Button>
                         </form>
                     </div>
                 </div>
+                <div>
+
+                    <Modal isOpen={modal} toggle={toggle} >
+                        <ModalHeader toggle={toggle}>Create Account</ModalHeader>
+                        <ModalBody>
+                            <div>
+                                <Input
+                                    placeholder="Name"
+                                    onChange={handleNameChange}
+                                />
+                                <Input
+                                    placeholder="Email"
+                                    onChange={handleEmailChange}
+                                />
+                                <Input
+                                    placeholder="Password"
+                                    onChange={handlePasswordChange}
+                                />
+                            </div>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="primary" onClick={() => {toggle(); addUser(name, email, password)}}>
+                                Add
+                            </Button>{' '}
+                            <Button color="secondary" onClick={toggle}>
+                                Cancel
+                            </Button>
+                        </ModalFooter>
+                    </Modal>
+                </div>
             </div>
+
         )
 };
 
